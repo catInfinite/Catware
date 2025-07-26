65783b0440f7b5cd63a93651218323497aa04ea9
catInfinite <verysillycat83@gmail.com>
2025-07-26 17:18:47 +0700

    not retard mb

--- loader.lua
+++ loader.lua
@@ -1,6 +1,6 @@
 local scripts = {
-    [126509999114328] = "https://raw.githubusercontent.com/catInfinite/Catware/main/games/99NightsInDiddyParty.lua", -- 99 days in forest
-    [443406476] = "https://raw.githubusercontent.com/catInfinite/Catware/main/games/ProjectLazarus.lua" -- project lazarus
+    [126509999114328] = "https://raw.githubusercontent.com/catInfinite/Catware/refs/heads/main/games/99NightsInDiddyParty.lua", -- 99 days in forest
+    [443406476] = "https://raw.githubusercontent.com/catInfinite/Catware/refs/heads/main/games/ProjectLazarus.lua" -- project lazarus
 }
 
 local placeId = game.PlaceId
